94ecdd2e4240260923d9f449ab257f440d953e94
justinadjiembaks <justin_adjiembaks@hotmail.com>
2017-10-18 21:17:39 +0200

    Created CreateUser and Login Screen

---
+++ app/src/main/java/com/example/justi/ownapp/LogInActivity.java
@@ -0,0 +1,82 @@
+package com.example.justi.ownapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogInActivity extends AppCompatActivity {
+
+    String email1, password1;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Signed In", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Signed Out", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+    public void logIn(View view){
+
+        EditText et1mail = (EditText) findViewById(R.id.editemail1);
+        EditText et1password= (EditText)findViewById(R.id.editpassword1);
+        email1 = et1mail.getText().toString();
+        password1 = et1password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email1, password1)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Succesful", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("Logging in Failed", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LogInActivity.this, "Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        } else{
+                            Toast.makeText(LogInActivity.this, "Succesfuly logged in as " + email1,
+                                    Toast.LENGTH_SHORT).show();
+                            searchscreen();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+    public void searchscreen(){
+
+        Intent search = new Intent(this, SearchActivity.class);
+        startActivity(search);
+        finish();
+    }
+}
--- app/src/main/java/com/example/justi/ownapp/MainActivity.java
+++ app/src/main/java/com/example/justi/ownapp/MainActivity.java
@@ -1,22 +1,108 @@
 package com.example.justi.ownapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Signed in", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Signed out", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+    public void goToLogIn(View view){
+        Intent logInIntent = new Intent(this,LogInActivity.class);
+        startActivity(logInIntent);
+        finish();
+    }
+
+    public void goToLogIn1(){
+        Intent logInIntent = new Intent(this,LogInActivity.class);
+        startActivity(logInIntent);
+        finish();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void createUser(View view){
+
+        EditText etemail = (EditText) findViewById(R.id.editemail);
+        EditText etpassword = (EditText)findViewById(R.id.editpassword);
+        email = etemail.getText().toString();
+        password = etpassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Create user", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Created New User Failed",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Created user " + email,
+                                    Toast.LENGTH_SHORT).show();
+                            goToLogIn1();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+
     }
 }
